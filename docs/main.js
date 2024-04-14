7eb29d232e1c3402a33d7c86c103af8c9bf344d0
Abdullah Jebiro <81176026+Abdullah-Jebiro@users.noreply.github.com>
2024-04-14 10:46:14 +0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -5128,7 +5128,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 const environment = {
   production: false,
-  apiUrl: 'https://localhost:7209/api/'
+  apiUrl: 'http://reservio-api.runasp.net/api/'
 };
 
 /***/ }),
